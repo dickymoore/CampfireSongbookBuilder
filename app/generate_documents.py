c9e49959b56232909f1049ad966595f04a02f3fe
dickymoorebjss <128724856+dickymoorebjss@users.noreply.github.com>
2024-06-26 16:41:20 +0100

    document generation working

--- app/generate_documents.py
+++ app/generate_documents.py
@@ -1,5 +1,5 @@
 from docx import Document
-import time
+import re
 import logging
 from app.fetch_data import get_lyrics_from_genius
 from app.cache import cache_data, load_cache
@@ -7,13 +7,35 @@ from app.cache import cache_data, load_cache
 # Configure logging
 logger = logging.getLogger(__name__)
 
-def remove_contributors_and_embeds(document):
-    """Remove paragraphs containing 'Contributors' and 'Embed' from the document."""
-    new_doc = Document()
-    for para in document.paragraphs:
-        if "Contributors" not in para.text and "Embed" not in para.text:
-            new_doc.add_paragraph(para.text)
-    return new_doc
+def remove_contributors_and_embeds(lyrics):
+    lyrics = re.sub(r'^.*Contributors', '', lyrics, flags=re.DOTALL)
+    lyrics = re.sub(r'Embed\s*$', '', lyrics, flags=re.MULTILINE)
+    return lyrics
+
+def get_song_lyrics_info(song_list, genius_client):
+    lyrics_cache = load_cache('data/cache/lyrics_cache.json')
+    song_info = []
+
+    for song in sorted(song_list, key=lambda x: x['Title']):
+        artist = song['Artist']
+        title = song['Title']
+        cache_key = f"{artist} - {title}"
+        
+        if cache_key in lyrics_cache and bool(lyrics_cache[cache_key]) and lyrics_cache[cache_key] != "Lyrics not found.":
+            lyrics = lyrics_cache[cache_key]
+        else:
+            lyrics = get_lyrics_from_genius(title, artist, genius_client)
+            if bool(lyrics) and lyrics != "Lyrics not found.":
+                lyrics_cache[cache_key] = lyrics
+                cache_data('data/cache/lyrics_cache.json', lyrics_cache)  # Update the cache file immediately
+            else:
+                lyrics = "Lyrics not found."
+
+        cleaned_lyrics = remove_contributors_and_embeds(lyrics)
+        num_characters = len(cleaned_lyrics)
+        song_info.append((title, num_characters))
+
+    return song_info
 
 def generate_documents(song_list, genius_client, lyrics_output, chords_output):
     logger.info("Loading cache data...")
@@ -31,26 +53,33 @@ def generate_documents(song_list, genius_client, lyrics_output, chords_output):
         cache_key = f"{artist} - {title}"
         if cache_key in lyrics_cache:
             lyrics = lyrics_cache[cache_key]
-            logger.debug("Lyrics loaded from cache.")
+            if bool(lyrics):
+                logger.debug("Lyrics loaded from cache.")
+            else: 
+                logger.debug("Attampt to load lyrics from cache failed.")
         else:
             lyrics = get_lyrics_from_genius(title, artist, genius_client)
-            lyrics_cache[cache_key] = lyrics
-            cache_data('data/cache/lyrics_cache.json', lyrics_cache)  # Update the cache file immediately
-            logger.debug("Lyrics fetched and cached.")
+            if lyrics and lyrics != "Lyrics not found.":
+                lyrics_cache[cache_key] = lyrics
+                cache_data('data/cache/lyrics_cache.json', lyrics_cache)  # Update the cache file immediately
+                logger.debug("Lyrics fetched and cached.")
+            else:
+                lyrics = "Lyrics not found."
+                logger.debug("Lyrics not found.")
 
+        cleaned_lyrics = remove_contributors_and_embeds(lyrics)
         lyrics_document.add_heading(f"{title} by {artist}", level=1)
-        lyrics_document.add_paragraph(lyrics)
-        lyrics_document.add_page_break()
+        # table = lyrics_document.add_table(rows=1, cols=1)
+        # row = table.rows[0]
+        # cell_lyrics = row.cells[0]
+        # cell_lyrics.text = cleaned_lyrics
+        lyrics_document.add_paragraph(cleaned_lyrics)
+        # lyrics_document.add_page_break()
 
         # chords_document.add_heading(f"{title} by {artist}", level=1)
         # chords_document.add_paragraph(chords)
         # chords_document.add_page_break()
-
-        time.sleep(5)
-
-    # Remove Contributors and Embed sections before saving
-    cleaned_lyrics_document = remove_contributors_and_embeds(lyrics_document)
-    cleaned_lyrics_document.save(lyrics_output)
+    lyrics_document.save(lyrics_output)
     logger.info(f"Lyrics document saved as {lyrics_output}.")
 
     # chords_document.save(chords_output)
--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 import pandas as pd
 import json
 import logging
-from app.generate_documents import generate_documents
+import argparse
+from app.generate_documents import generate_documents, get_song_lyrics_info
 from app.fetch_data import get_genius_client
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Load configuration
-logging.info("Loading configuration...")
-with open('data/config/config.json', 'r') as config_file:
-    config = json.load(config_file)
 
-access_token = config['genius']['client_access_token']
+def main():
+    # Argument parser setup
+    parser = argparse.ArgumentParser(description="Process some songs.")
+    parser.add_argument('--get-song-info', action='store_true', help='Get song titles and the character length of the lyrics')
+    args = parser.parse_args()
 
-# Initialize Genius client
-logging.info("Initializing Genius client...")
-genius_client = get_genius_client(access_token)
+    # Load configuration
+    logging.info("Loading configuration...")
+    with open('data/config/config.json', 'r') as config_file:
+        config = json.load(config_file)
 
-# Load songs from CSV file
-csv_file = 'data/src/CampfireSongs.csv'
-logging.info(f"Loading songs from {csv_file}...")
-try:
-    songs_df = pd.read_csv(csv_file)
-    songs = songs_df.to_dict('records')
-except Exception as e:
-    logging.error(f"Failed to load songs: {e}")
-    raise
+    access_token = config['genius']['client_access_token']
 
-lyrics_output = "data/output/Lyrics_Document.docx"
-chords_output = "data/output/Chords_Document.docx"
+    # Initialize Genius client
+    logging.info("Initializing Genius client...")
+    genius_client = get_genius_client(access_token)
 
-logging.info("Generating documents...")
-generate_documents(songs, genius_client, lyrics_output, chords_output)
-logging.info("Documents generated successfully.")
+    # Load songs from CSV file
+    csv_file = 'data/src/CampfireSongs.csv'
+    logging.info(f"Loading songs from {csv_file}...")
+    try:
+        songs_df = pd.read_csv(csv_file)
+        songs = songs_df.to_dict('records')
+    except Exception as e:
+        logging.error(f"Failed to load songs: {e}")
+        raise
+
+    if args.get_song_info:
+        song_info = get_song_lyrics_info(songs, genius_client)
+        for title, num_characters in song_info:
+            print(f"{title}: {num_characters} characters")
+    else:
+        lyrics_output = "data/output/Lyrics_Document.docx"
+        chords_output = "data/output/Chords_Document.docx"
+        
+        logging.info("Generating documents...")
+        generate_documents(songs, genius_client, lyrics_output, chords_output)
+        logging.info("Documents generated successfully.")
+
+if __name__ == "__main__":
+    main()
