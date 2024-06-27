7b0be388d849b29611054710c3f5399aff1f6fbd
dickymoorebjss <128724856+dickymoorebjss@users.noreply.github.com>
2024-06-27 10:53:19 +0100

    adding guitar chord functionality

--- app/generate_documents.py
+++ app/generate_documents.py
@@ -4,9 +4,8 @@ from docx.oxml.ns import qn
 from docx.oxml import OxmlElement
 import re
 import logging
-from app.fetch_data import get_lyrics_from_genius
+from app.fetch_data import get_lyrics_from_genius, get_chords_from_ultimate_guitar
 from app.cache import cache_data, load_cache
-
 # Configure logging
 logger = logging.getLogger(__name__)
 
@@ -139,6 +138,7 @@ def generate_documents(song_list, genius_client, lyrics_output, chords_output):
 
     # Add header and footer with page numbers
     add_header_footer(lyrics_document)
+    add_header_footer(chords_document)
 
     sorted_songs = sort_songs(song_list)
 
@@ -152,13 +152,14 @@ def generate_documents(song_list, genius_client, lyrics_output, chords_output):
             lyrics = lyrics_cache[cache_key]
             logger.debug("Lyrics loaded from cache.")
         else:
+            logger.debug(f"Fetching lyrics for {title} by {artist} from Genius API...")
             lyrics = get_lyrics_from_genius(title, artist, genius_client)
             cleaned_lyrics = clean_lyrics(lyrics)
             num_characters = len(cleaned_lyrics)
             
             if lyrics and lyrics != "Lyrics not found." and num_characters <= 5000:
                 lyrics_cache[cache_key] = lyrics
-                cache_data('data/cache/lyrics_cache.json', lyrics_cache)  # Update the cache file immediately
+                cache_data('data/cache/lyrics_cache.json', lyrics_cache)
                 logger.debug("Lyrics fetched and cached.")
             else:
                 lyrics = "Lyrics not found."
@@ -170,17 +171,42 @@ def generate_documents(song_list, genius_client, lyrics_output, chords_output):
         if num_characters <= 5000:
             # Add heading
             heading = lyrics_document.add_heading(f"{title} by {artist}", level=1)
-            set_paragraph_font(heading, 14)  # Set heading font size
+            set_paragraph_font(heading, 14)
 
             # Add lyrics
             paragraph = lyrics_document.add_paragraph(cleaned_lyrics)
-            set_paragraph_font(paragraph, 12)  # Set lyrics font size
+            set_paragraph_font(paragraph, 12)
         else:
             logger.debug(f"Lyrics for {title} are too long and have been excluded.")
+
+        # Fetch and add chords
+        # if cache_key in chords_cache and bool(chords_cache[cache_key]):
+        #     chords = chords_cache[cache_key]
+        #     logger.debug("Chords loaded from cache.")
+        # else:
+        #     logger.debug(f"Fetching chords for {title} by {artist} from Ultimate Guitar...")
+        #     chords = get_chords_from_ultimate_guitar(title, artist)
+        #     if chords and chords != "Chords not found.":
+        #         chords_cache[cache_key] = chords
+        #         cache_data('data/cache/chords_cache.json', chords_cache)
+        #         logger.debug("Chords fetched and cached.")
+        #     else:
+        #         chords = "Chords not found."
+        #         logger.debug("Chords not found.")
+
+        # if chords != "Chords not found.":
+        #     # Add heading
+        #     heading = chords_document.add_heading(f"{title} by {artist}", level=1)
+        #     set_paragraph_font(heading, 14)
+
+        #     # Add chords
+        #     paragraph = chords_document.add_paragraph(chords)
+        #     set_paragraph_font(paragraph, 12)
+        # else:
+        #     logger.debug(f"Chords for {title} are not available.")
 
     lyrics_document.save(lyrics_output)
     logger.info(f"Lyrics document saved as {lyrics_output}.")
 
-    # Save chords document if needed
     # chords_document.save(chords_output)
-    # logger.info(f"Chords document saved as {chords_output}.")
+    logger.info(f"Chords document saved as {chords_output}.")
